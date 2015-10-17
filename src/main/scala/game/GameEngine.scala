b414b9accc2f277ac416ac5b5985024dea24828b
Josef Vlach <vlach.josef@gmail.com>
2015-10-17 19:37:24 +0100

    Proposal for further improvement alias Let's use State Monad

--- src/main/scala/game/GameEngine.scala
+++ src/main/scala/game/GameEngine.scala
@@ -15,6 +15,24 @@ class GameEngine {
 
   def generatePirate = generatePlayer(3)
 
+  /**
+   * To improve code and to get rid of evil var variables we can use so called State monad.
+   *
+   * Concept of State Monad is similar to concept of Reader and Writer monad. For getting familiar with all these concepts
+   * and to learn to recognize when they can be applied there exists this great presentation by Mark Hibberd:
+   * https://www.youtube.com/watch?v=4KvRUBv7-Us
+   *
+   * Slide for this presentation are here:
+   * http://mth.io/slides/patterns-in-types-ylj
+   *
+   * This talk was accompanied by workshop which source code is available here:
+   * https://github.com/markhibberd/lambdajam-patterns-in-types
+   *
+   * It is highly recommended to go through this workshop and try to implement all challenges
+   *
+   * For completeness sake here is link to Mark Hibberd's website with description of his talk mentioned above:
+   * http://mth.io/talks/patterns-in-types-ylj
+   */
   var player: Player = generatePlayer(howMany = 2)
   var pirate: Player = _
 
